a5c7702885fb0ed898e8cfa8882facb30d043f5f
Mohammad Zeineddine <mohammad.zeineddine50@gmail.com>
2025-03-05 17:08:51 +0200

    Adding http for ip

--- Wallet-Server/user/v1/register.php
+++ Wallet-Server/user/v1/register.php
@@ -2,7 +2,7 @@
     include("../../connection/connection.php");
     include("../../utils.php");
     include("../../models/user.php");
-    $base1 = "13.38.107.39";
+    $base1 = "http://13.38.107.39";
     $base = $base1."/Digital-Wallet/Wallet-Server/user/v1";
     if ($_SERVER['CONTENT_TYPE'] === 'application/json') {
         $data = json_decode(file_get_contents('php://input'), true);
